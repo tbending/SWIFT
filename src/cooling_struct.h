68c13e3b114758278771cf2c9a50403eb5e2869e
Joey <joeybraspenning@gmail.com>
2021-09-16 18:37:26 +0200

    linked cooling files

--- src/cooling_struct.h
+++ src/cooling_struct.h
@@ -34,6 +34,8 @@
 #include "./cooling/const_du/cooling_struct.h"
 #elif defined(COOLING_CONST_LAMBDA)
 #include "./cooling/const_lambda/cooling_struct.h"
+#elif defined(COOLING_PIECEWISE_POWERLAW)
+#include "./cooling/variable_lambda/cooling_struct.h"
 #elif defined(COOLING_COMPTON)
 #include "./cooling/Compton/cooling_struct.h"
 #elif defined(COOLING_GRACKLE)
